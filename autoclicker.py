9b50e5df33c9a582ae9f425c2272f0c73c9808d4
Lvniewski111 <120188281+Lvniewski111@users.noreply.github.com>
2022-12-09 14:44:14 +0100

    Update autoclicker.py
    UI update+ actual place to put clicking functions instead of click simulation in shell

--- autoclicker.py
+++ autoclicker.py
@@ -1,64 +1,48 @@
+import tkinter as tk
 import time
-from tkinter import *
 
-# Set the number of clicks
-num_clicks = 0
-
-# Set the delay between clicks (in seconds)
-delay = 0.5
-
-# Function to start the auto clicker
+# define the start_clicking function
 def start_clicking():
-    global num_clicks
-    num_clicks = 100
-    perform_clicks()
+    global is_clicking
+    is_clicking = True
 
-# Function to stop the auto clicker
+    # start a loop that simulates clicking every half second
+    while is_clicking:
+        time.sleep(0.5)
+        # simulate a mouse click
+        # (replace this with the actual code to click the mouse)
+        print("click!")
+
+# define the stop_clicking function
 def stop_clicking():
-    global num_clicks
-    num_clicks = 0
+    global is_clicking
+    is_clicking = False
 
-# Function to perform the clicks
-def perform_clicks():
-    global num_clicks
-    while num_clicks > 0:
-        # Use the time module to pause execution for the specified
-        # delay
-        time.sleep(delay)
+# create the root window
+root = tk.Tk()
+root.title("AutoClicker")
 
-        # Simulate a click
-        print("click")
-        num_clicks -= 1
+# set the minimum size of the window
+root.minsize(400, 400)
 
-# Create the GUI
-root = Tk()
-root.title("Auto Clicker")
+# create the main frame
+frame = tk.Frame(root)
+frame.pack()
 
-# Bind the keyboard shortcuts Ctrl+Shift+S and Ctrl+Shift+E
-# to the start and stop buttons, respectively
-root.bind("<Control-Shift-s>", lambda event: start_clicking())
-root.bind("<Control-Shift-e>", lambda event: stop_clicking())
-
-# Create the start button
-start_button = Button(root, text="Start (Ctrl+Shift+S)", command=start_clicking)
+# create the start button and assign it the keyboard shortcut "s"
+start_button = tk.Button(frame, text="Start", command=start_clicking)
 start_button.pack()
+root.bind("<s>", start_clicking)
 
-# Create the stop button
-stop_button = Button(root, text="Stop (Ctrl+Shift+E)", command=stop_clicking)
+# create the stop button and assign it the keyboard shortcut "t"
+stop_button = tk.Button(frame, text="Stop", command=stop_clicking)
 stop_button.pack()
+root.bind("<t>", stop_clicking)
 
-# Start the GUI event loop
+# create the quit button and assign it the keyboard shortcut "q"
+quit_button = tk.Button(frame, text="Quit", command=root.quit)
+quit_button.pack()
+root.bind("<q>", root.quit)
+
+# start the GUI event loop
 root.mainloop()
-
-
-
-"""
-This code will create a GUI with two buttons, "Start" and "Stop". When the "Start" button is clicked,
-or the keyboard shortcut Ctrl+Shift+S is pressed, the auto clicker will perform 100 clicks with a delay of 0.5 seconds
-between each click. You can modify the num_clicks and delay variables to change the number of clicks and the delay between
-clicks, respectively. The "Stop" button, or the keyboard shortcut Ctrl+Shift+E, will stop the auto clicking.
-
-Please note that this code is just a simulation of an auto clicker and will not actually interact with anything on
-your computer. It just prints "click" to the console to simulate
-
-"""
