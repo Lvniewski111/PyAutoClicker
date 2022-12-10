f05afb90446276ffcddf00efcaf72a26097e1ecb
Lvniewski111 <120188281+Lvniewski111@users.noreply.github.com>
2022-12-10 22:11:13 +0100

    Update autoclicker.py
    Added current mouse position tracking

--- autoclicker.py
+++ autoclicker.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import time
+import pyautogui
 
 # define the start_clicking function
 def start_clicking():
@@ -8,8 +9,8 @@ def start_clicking():
 
     # start a loop that simulates clicking every half second
     while is_clicking:
-        time.sleep(0.5)
-        # simulate a mouse click
+        time.sleep(1.0)
+        pyautogui.displayMousePosition()
         # (replace this with the actual code to click the mouse)
         print("click!")
 
@@ -23,24 +24,24 @@ root = tk.Tk()
 root.title("AutoClicker")
 
 # set the minimum size of the window
-root.minsize(400, 400)
+root.minsize(400, 200)
 
 # create the main frame
 frame = tk.Frame(root)
 frame.pack()
 
 # create the start button and assign it the keyboard shortcut "s"
-start_button = tk.Button(frame, text="Start", command=start_clicking)
+start_button = tk.Button(frame, text="START", command=start_clicking, font= ('Helvetica 20 bold'))
 start_button.pack()
 root.bind("<s>", start_clicking)
 
 # create the stop button and assign it the keyboard shortcut "t"
-stop_button = tk.Button(frame, text="Stop", command=stop_clicking)
+stop_button = tk.Button(frame, text="STOP", command=stop_clicking, font= ('Helvetica 20 bold'))
 stop_button.pack()
 root.bind("<t>", stop_clicking)
 
 # create the quit button and assign it the keyboard shortcut "q"
-quit_button = tk.Button(frame, text="Quit", command=root.quit)
+quit_button = tk.Button(frame, text="QUIT", command=root.quit, font= ('Helvetica 20 bold'))
 quit_button.pack()
 root.bind("<q>", root.quit)
 
